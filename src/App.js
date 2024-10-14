4bdbf53477620b4b818f1194ad3d6ec3359846c6
lamayig23@gmail.com <lamayig23@gmail.com>
2024-10-14 15:36:03 +0300

    ci/cd pepline Test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import ThemeCustomization from "./themes";
 import ThemeRoutes from "./routes";
+import Typography from "@mui/material/Typography";
 
 const App = () => {
     return (
         <ThemeCustomization>
             <ThemeRoutes/>
         </ThemeCustomization>
+        <Typography>Test Ci/Cd Peplines</Typography>
     )
 };
 export default App;
